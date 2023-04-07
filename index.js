ace68883665217e273cb87117ca40a705f26a358
monsterking1 <123229020+monsterking1@users.noreply.github.com>
2023-04-07 17:17:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+var slideIndex = 0;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+showSlide();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// var timer = setInterval(showSlide, 2000);
+
+var slideContainer = document.getElementById('slideshow-container');
+var slideControl = document.getElementById('slide-control');
+
+slideContainer.onmouseover = function() {
+	clearInterval(timer);
+}
+
+slideControl.onmouseover = function() {
+	clearInterval(timer);
+}
+
+slideContainer.onmouseleave = function() {
+	// timer = setInterval(showSlide, 2000);
+}
+
+slideControl.onmouseleave = function() {
+	// timer = setInterval(showSlide, 2000);
+}
+
+function showSlide() {
+	var i;
+	var slides = document.getElementsByClassName('slide-item');
+	var dots = document.getElementsByClassName('dot');
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	for (var i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) slideIndex = 1;
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+}
+
+function chooseSlide(n) {
+	slideIndex = n;
+	showSlide();
+	clearInterval(timer);
+}
+
+function showMenu() {
+	var topNav = document.getElementById('topnav');
+	if (topNav.className === "navbar") {
+		topNav.className += " show";
+	} else {
+		topNav.className = "navbar";
+	}
+}
